a837d356743facca268ed4dcd4aaf07316c6b1c6
Caleb Stevenson <cgrahamstevenson@gmail.com>
2017-02-08 13:08:19 -0800

    add method to set gridview to gone when autocompleteEditText takes focus

--- app/src/main/java/com/doandstevensen/lifecollage/ui/main/MainActivity.java
+++ app/src/main/java/com/doandstevensen/lifecollage/ui/main/MainActivity.java
@@ -1,12 +1,22 @@
 package com.doandstevensen.lifecollage.ui.main;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.doandstevensen.lifecollage.R;
 import com.doandstevensen.lifecollage.data.model.User;
@@ -22,13 +32,20 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnItemSelected;
 import io.realm.RealmResults;
 
 public class MainActivity extends BaseActivity implements MainContract.MvpView, MainSearchAdapter.ClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    
     @BindView(R.id.gridView)
     GridView gridView;
     @BindView(R.id.autoCompleteTextView)
     AutoCompleteTextView autoCompleteTextView;
+    @BindView(R.id.linearLayout)
+    LinearLayout linearLayout;
+    InputMethodManager imm;
+
 
     private MainPresenter mPresenter;
 
@@ -37,17 +54,21 @@ public class MainActivity extends BaseActivity implements MainContract.MvpView, 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imm = (InputMethodManager) this
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+
         ButterKnife.bind(this);
 
         mPresenter = new MainPresenter(this);
         mPresenter.getGridViewUsers();
         mPresenter.searchUsers();
 
+        setupAutoCompleteTextViewListeners();
+
         String currentUserId = RealmUserManager.getCurrentUserId();
         if (currentUserId != null) {
             navigateToCollageList(currentUserId);
         }
-
     }
 
     public void setupGridViewAdapter(final ArrayList<User> featuredUsers) {
@@ -71,6 +92,36 @@ public class MainActivity extends BaseActivity implements MainContract.MvpView, 
 
     public void clearSearchView() {
         autoCompleteTextView.setText("");
+        autoCompleteTextView.clearFocus();
+    }
+
+    //TODO set gridView to visible when edit text clears focus
+
+    public void setupAutoCompleteTextViewListeners() {
+        autoCompleteTextView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (imm.isAcceptingText()) {
+                    Log.d(TAG, "Software keyboard visible");
+                    gridView.setVisibility(View.GONE);
+                    linearLayout.setGravity(Gravity.NO_GRAVITY);
+                } else {
+                    Log.d(TAG, "Software keyboard not visible");
+                }
+            }
+        });
+
+        autoCompleteTextView.setOnEditorActionListener( new AutoCompleteTextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_SEARCH) {
+                    Log.d(TAG, "SEARCHING FOR USERS");
+                    autoCompleteTextView.clearFocus();
+                }
+                return true;
+            }
+        });
     }
 
     private void navigateToCollage(String uid, String name) {
@@ -100,6 +151,8 @@ public class MainActivity extends BaseActivity implements MainContract.MvpView, 
         Intent intent = new Intent(getBaseContext(), LogInActivity.class);
         startActivity(intent);
     }
+
+
 
     @Override
     public void onDestroy() {
