fcd5af77e54267a64d847c4ebc695cf0cedf4a97
Sheena Nickerson <sheenanick@gmail.com>
2017-02-19 13:28:51 -0800

    Delete Account dialog fragment and optimize imports

--- app/src/main/java/com/doandstevensen/lifecollage/ui/collage_list/CollageListActivity.java
+++ app/src/main/java/com/doandstevensen/lifecollage/ui/collage_list/CollageListActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
