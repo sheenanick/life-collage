dad498710266fbb4495e513350d0fcbc23d168a2
Sheena Nickerson <sheenanick@gmail.com>
2017-02-19 16:22:01 -0800

    Load three collages into grid view from API

--- app/src/main/java/com/doandstevensen/lifecollage/ui/main/MainActivity.java
+++ app/src/main/java/com/doandstevensen/lifecollage/ui/main/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 
 import com.doandstevensen.lifecollage.R;
 import com.doandstevensen.lifecollage.data.model.ApplicationToken;
+import com.doandstevensen.lifecollage.data.model.PictureResponse;
 import com.doandstevensen.lifecollage.data.remote.DataManager;
 import com.doandstevensen.lifecollage.ui.base.BaseActivity;
 import com.doandstevensen.lifecollage.ui.collage_detail.CollageActivity;
@@ -20,6 +21,9 @@ import com.doandstevensen.lifecollage.ui.collage_list.CollageListActivity;
 import com.doandstevensen.lifecollage.ui.signin.LogInActivity;
 import com.doandstevensen.lifecollage.ui.search.SearchResultsActivity;
 import com.doandstevensen.lifecollage.ui.signup.SignUpActivity;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,6 +38,9 @@ public class MainActivity extends BaseActivity implements MainContract.MvpView {
     SearchView searchView;
     public static final String TAG = MainActivity.class.getSimpleName();
     private MainPresenter mPresenter;
+    private MainImageAdapter mAdapter;
+    private ArrayList<PictureResponse> mPictures;
+    private String[] mCollageTitles = {"My Dog Collage", "Winter Wonderland", "Food Collage", "Travel Collage", "Pretty Flowers", "Good Times"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +58,7 @@ public class MainActivity extends BaseActivity implements MainContract.MvpView {
         setupGridViewAdapter();
 
         mPresenter = new MainPresenter(this, this);
+        mPresenter.getGridViewUsers();
     }
 
     private void initSearchView() {
@@ -61,21 +69,26 @@ public class MainActivity extends BaseActivity implements MainContract.MvpView {
     }
 
     public void setupGridViewAdapter() {
-        MainImageAdapter adapter = new MainImageAdapter(this);
-        gridView.setAdapter(adapter);
+        mAdapter = new MainImageAdapter(this);
+        gridView.setAdapter(mAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                //TODO navigate to collage activity on click
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                int collageId = mPictures.get(position).getCollageId();
+                String title = mCollageTitles[position];
+                navigateToCollage(collageId, title);
             }
         });
     }
 
-    private void navigateToCollage(String collageId, String name) {
-        Intent intent = new Intent(getBaseContext(), CollageActivity.class);
-        intent.putExtra("name", name);
-        intent.putExtra("collageId", collageId);
-        startActivity(intent);
+    @Override
+    public void updateGridView(ArrayList<PictureResponse> pictures) {
+        mPictures = pictures;
+        mAdapter.setPictures(pictures);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void navigateToCollage(int collageId, String name) {
+
     }
 
     @Override
